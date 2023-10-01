fe27b68f194cdb6629cd5c51acf347338641488f
ththihn <145424802+ththihn@users.noreply.github.com>
2023-10-01 22:48:56 +0700

    Add files via upload

---
+++ deadline1/Deadline1.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.deadline1;
+
+/**
+ *
+ * @author FPTSHOP
+ */
+public class Deadline1 {
+
+    public static void main(String[] args) {
+    
+}
